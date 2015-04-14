f209b31dd116b8600810b9f318edc7076b79344c
maxlweaver <maxlweaver@gmail.com>
2015-04-14 11:19:59 +0100

    Add increase temp test

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,16 @@
 describe('Thermostat', function() {
 
+  var thermostat;
+
   it('should have a starting temperature of 20', function() {
     thermostat = new Thermostat();
-    expect(thermostat.temperature).toEqual(20);
-  }); 
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  it('should increase temperature by one', function() {
+    thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.temperature()).toEqual(21);
+  });
 
 });
---
+++ src/Thermostat.js
@@ -0,0 +1,9 @@
+var Thermostat = function(){
+
+  var temperature = 20;
+  
+};
+
+Thermostat.prototype.temperature = function() {
+  return 20;
+};
